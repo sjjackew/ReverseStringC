1ce5206f59cd16b96e16db859e37f6ac948c2cc0
sjjackew <sjjackew@outlook.com>
2019-12-27 12:04:59 -0500

    first commit

---
+++ ReverseStringC/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseStringC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "all cows eat grass";
+            string resultString = string.Join(" ", inputString
+                .Split(' ')
+                .Select(x => new String(x.Reverse().ToArray())));
+
+            Console.WriteLine(resultString);
+
+        }
+    }
+}
